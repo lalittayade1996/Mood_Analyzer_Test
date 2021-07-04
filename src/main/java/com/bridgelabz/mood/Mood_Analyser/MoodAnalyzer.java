e41dc644e6a92aa264bda650c9108585b2654b9c
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-04 12:47:08 +0530

    Added : Sad Message Test With Refactor

--- src/main/java/com/bridgelabz/mood/Mood_Analyser/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/mood/Mood_Analyser/MoodAnalyzer.java
@@ -1,6 +1,15 @@
 package com.bridgelabz.mood.Mood_Analyser;
 
 public class MoodAnalyzer {
+	public String message;
+
+	public MoodAnalyzer() {
+	}
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
 	public String moodAnalyzer(String message) {
 		return "SAD";
 	}
